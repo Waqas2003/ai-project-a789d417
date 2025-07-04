8bbb0ab0e9123eb8159909a85e80b6b8f87af61c
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 01:16:48 +0500

    Add src\components\Footer.js

---
+++ src\components\Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function FooterComponent() {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">Copyright &copy; 2023 Pakistan Atomic Power</p>
+      </div>
+    </footer>
+  );
+}
+
+export default FooterComponent;

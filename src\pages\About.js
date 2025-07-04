c60b0d85d7194caf8cc0a3611cbecfffaf5de184
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 01:16:51 +0500

    Add src\pages\About.js

---
+++ src\pages\About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AboutComponent() {
+  return (
+    <div className="container">
+      <h1>About Us</h1>
+      <p>
+        Pakistan Atomic Power is committed to providing clean and sustainable energy solutions.
+      </p>
+    </div>
+  );
+}
+
+export default AboutComponent;

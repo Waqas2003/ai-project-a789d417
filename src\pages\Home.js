3d4ef72ebe9abfdab4b16281118e7227978ea64a
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 01:16:55 +0500

    Add src\pages\Home.js

---
+++ src\pages\Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HomeComponent() {
+  return (
+    <div className="container">
+      <h1>Welcome to Pakistan Atomic Power</h1>
+      <p>
+        Pakistan Atomic Power is a leading organization in the field of nuclear energy.
+      </p>
+    </div>
+  );
+}
+
+export default HomeComponent;

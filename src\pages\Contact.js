f9ebc02424ede2dc334163c4a519f75ee23e4542
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 01:16:53 +0500

    Add src\pages\Contact.js

---
+++ src\pages\Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function ContactComponent() {
+  return (
+    <div className="container">
+      <h1>Contact Us</h1>
+      <p>
+        Get in touch with us for more information.
+      </p>
+      <ul>
+        <li>Phone: +92 51 111 222 333</li>
+        <li>Email: [info@pakistanatomicpower.com](mailto:info@pakistanatomicpower.com)</li>
+        <li>Address: Islamabad, Pakistan</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ContactComponent;

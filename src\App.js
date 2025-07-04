3494e59626df77fc424fb0e87011c5014078548b
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 01:16:42 +0500

    Add src\App.js

---
+++ src\App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
